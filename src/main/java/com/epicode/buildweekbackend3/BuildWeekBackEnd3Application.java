ffa0b0b344d5ab4812c22c29496e5e231c7a92da
Cozzula-PIAN-Angelo <cozzulaangelo@gmail.com>
2026-08-31 11:50:34 +0200

    setup progetto Spring Boot

---
+++ src/main/java/com/epicode/buildweekbackend3/BuildWeekBackEnd3Application.java
@@ -0,0 +1,13 @@
+package com.epicode.buildweekbackend3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeekBackEnd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeekBackEnd3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicode/buildweekbackend3/BuildWeekBackEnd3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.buildweekbackend3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeekBackEnd3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
